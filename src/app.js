7f1990017ab5891a25e92d20540988fc7a5d8966
Tudor Ilisoi <tudorilisoi@yahoo.com>
2020-07-10 23:17:27 +0300

    added a sample join
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const knex = require('./db')()
 
 const app = express()
 
@@ -15,8 +16,17 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
+app.get('/', async (req, res) => {
+    console.log(knex)
+    const records = await knex
+    .table('habit_records')
+    .innerJoin('habits', `habit_records.habit_id`, `habits.id`)
+    //filter by record date
+    .where('habit_records.date_completed', '2020-07-12 22:52:05')
+    .select(['habits.habit_name', 'habit_records.date_completed'])
+    res.send(JSON.stringify({records}))
+
+    // res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
---
+++ src/db.js
@@ -0,0 +1,7 @@
+const knex = require('knex')
+
+const db = () => knex({
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+})
+module.exports = db;
