97260f569122211eb967392c36c7d22fedd2d612
Tudor Ilisoi <tudorilisoi@yahoo.com>
2020-08-15 16:59:31 +0300

    filter by user
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/habit_records/habit_matrix_router.js
+++ src/habit_records/habit_matrix_router.js
@@ -1,10 +1,10 @@
-const path = require('path');
-const express = require('express');
-const HabitRecordsService = require('./habit_records-service');
-const HabitsService = require('../habits/habits-service');
-const { requireAuth } = require('../middleware/jwt-auth');
-const habitMatrixRouter = express.Router();
-const jsonParser = express.json();
+const path = require('path')
+const express = require('express')
+const HabitRecordsService = require('./habit_records-service')
+const HabitsService = require('../habits/habits-service')
+const { requireAuth } = require('../middleware/jwt-auth')
+const habitMatrixRouter = express.Router()
+const jsonParser = express.json()
 
 async function getHabitsForUser(knex, userID) {
     return knex('habits')
@@ -13,26 +13,34 @@ async function getHabitsForUser(knex, userID) {
 }
 
 async function getCheckedStatus(knex, { startDate, endDate, userID, habitID }) {
-    return knex('habit_records')
-        .where('habit_id', habitID)
+    const query = knex('habit_records')
+    if (habitID) {
+        query.where('habit_id', habitID)
+    }
+
+    query
+        .innerJoin('habits', `habit_records.habit_id`, `habits.id`)
+        .where('habits.user_id', userID)
+
+    query
         .whereRaw('DATE(date_completed)>=?::date', startDate)
         .whereRaw('DATE(date_completed)<=?::date', endDate)
         .groupBy('day')
         .orderBy('date_completed_max')
-        // .orderByRaw(`TO_DATE("day", 'YYYY-MM-DD')`)
         .select(
             // 'id',
 
             // this is used to sort the records in chronological order
             knex.raw(`MAX(date_completed) as date_completed_max`),
-            
+
             // count should be at MOST 1, otherwise you have duplicate records on the same day
-            knex.raw('count(id) as checked'),
-            
+            knex.raw('count(habit_records.id) as checked'),
+
             // format date with postgres
             knex.raw(`to_char("date_completed", 'YYYY-MM-DD') as day`),
         )
-
+    const data = await query
+    return data
 }
 
 
@@ -52,10 +60,18 @@ habitMatrixRouter
 
         // THIS GETS ALL HABIT records FROM A GIVEN USER
         const { id: userID } = req.user
-        const db = req.app.get('db');
+        const db = req.app.get('db')
         try {
+
             const habits = await getHabitsForUser(db, userID)
-            const data = await getCheckedStatus(db, { startDate, endDate, userID, habitID: 1 })
+            const data = await getCheckedStatus(db,
+                {
+                    startDate,
+                    endDate,
+                    userID,
+                    habitID: idFilter === 'all' ? null : idFilter
+                })
+
             res.json({
                 // habits, 
                 data,
@@ -63,8 +79,8 @@ habitMatrixRouter
 
         } catch (err) {
             console.error('err', err)
-            next();
-        };
+            next()
+        }
     })
 
 module.exports = habitMatrixRouter
