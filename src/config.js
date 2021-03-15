638b66a0db5b66c870d26c0387c8e7f98dae5ee7
Seth Anderson <sdande02@gmail.com>
2021-03-14 22:11:39 -0700

    trying ?ssl=true'

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:p@localhost/habitful',
+  DATABASE_URL: process.env.DATABASE_URL + '?ssl=true' || 'postgresql://dunder_mifflin:p@localhost/habitful',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:p@localhost/habitful-test',
   CLIENT_ORIGIN: process.env.NODE_ENV === 'production'
     ? process.env.CLIENT_ORIGIN
